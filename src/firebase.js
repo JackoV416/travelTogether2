6b59436068bcb336eb679eab909569dee2a602ea
JackoV416 <jamiekwok416@gmail.com>
2025-11-24 19:46:19 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,13 @@
-// src/firebase.js
-
 import { initializeApp } from 'firebase/app';
-// 引入 Authentication 服務和需要的函式
+// 引入 Authentication 服務和 Google 登入提供者
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'; 
 // 引入 Firestore 服務
 import { getFirestore } from 'firebase/firestore'; 
 
-// TODO: 請用你的 Firebase 專案設定替換下方內容 (保持不變)
+// TODO: 請用你的 Firebase 專案設定替換下方內容
 const firebaseConfig = {
-    @@ -13,10 +15,13 @@ const firebaseConfig = {
-      measurementId: "G-WB5T9XJ42E"
+@@ -13,10 +15,13 @@ const firebaseConfig = {
+  measurementId: "G-WB5T9XJ42E"
 };
 
 // 1. 初始化 Firebase 應用程式
@@ -20,5 +18,5 @@ export const auth = getAuth(app); // 認證服務
 export const googleAuthProvider = new GoogleAuthProvider(); // Google 登入提供者
 export const db = getFirestore(app); // Firestore 資料庫
 
-// 3. 匯出 Auth 相關函式供 App.jsx 和其他地方使用 (關鍵修正！)
-export { signInWithPopup, signOut };
+// 3. 匯出 Auth 相關函式供 App.jsx 和其他地方使用
+export { signInWithPopup, signOut }; 
