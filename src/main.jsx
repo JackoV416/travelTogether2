7d807d81f02e437cc72a6e47378c514e7b6368fe
JackoV416 <jamiekwok416@gmail.com>
2025-11-24 16:30:05 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+// 確保導入是單獨一行，並且路徑是正確的
+import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
