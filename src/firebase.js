81d389a7d1eb0290db052239e30286dc32573c5b
JackoV416 <jamiekwok416@gmail.com>
2025-11-24 19:33:11 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,14 @@
+// src/firebase.js
+
 import { initializeApp } from 'firebase/app';
-// 引入 Authentication 服務和 Google 登入提供者
+// 引入 Authentication 服務和需要的函式
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'; 
 // 引入 Firestore 服務
 import { getFirestore } from 'firebase/firestore'; 
 
-// TODO: 請用你的 Firebase 專案設定替換下方內容
+// TODO: 請用你的 Firebase 專案設定替換下方內容 (保持不變)
 const firebaseConfig = {
-  apiKey: "AIzaSyBGlEoflf06E_lBi7FHnU1k2xNRN3_QBes",
-  authDomain: "travel-together2-byjamie.firebaseapp.com",
-  projectId: "travel-together2-byjamie",
-  storageBucket: "travel-together2-byjamie.firebasestorage.app",
-  messagingSenderId: "270079374388",
-  appId: "1:270079374388:web:93aeeb483945dbab7ac661",
-  measurementId: "G-WB5T9XJ42E"
+    // ... 您的 Firebase 設定內容 ...
 };
 
 // 1. 初始化 Firebase 應用程式
@@ -23,5 +19,5 @@ export const auth = getAuth(app); // 認證服務
 export const googleAuthProvider = new GoogleAuthProvider(); // Google 登入提供者
 export const db = getFirestore(app); // Firestore 資料庫
 
-// 3. 匯出 Auth 相關函式供 App.jsx 和其他地方使用
-export { signInWithPopup, signOut }; 
+// 3. 匯出 Auth 相關函式供 App.jsx 和其他地方使用 (關鍵修正！)
+export { signInWithPopup, signOut };
