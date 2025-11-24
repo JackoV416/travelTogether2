ceae46ef816cf0e3f8cd560abbdf7e79ea0cd508
JackoV416 <jamiekwok416@gmail.com>
2025-11-24 16:21:03 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,16 +8,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
-
-6. src/index.css (引入 Tailwind)
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
-
-/* 引入字體 */
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@300;400;500;700&family=Zen+Kaku+Gothic+New:wght@300;400;500&display=swap');
-
-body {
-    /* 確保字體在 CSS 載入後生效 */
-    font-family: 'Zen Kaku Gothic New', 'Noto Sans TC', sans-serif;
-}
