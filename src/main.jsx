04464887702b546bcb61bd13aa1149aef5a55870
JackoV416 <jamiekwok416@gmail.com>
2025-11-24 18:28:06 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-// 確保這裡是乾淨的程式碼，沒有額外的文字或錯誤符號
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
