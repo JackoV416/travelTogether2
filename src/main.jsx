a9800b8e01f68f337e0d4ca3452c7b5b8bf1755c
JackoV416 <jamiekwok416@gmail.com>
2025-11-25 14:07:20 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,3 @@
-// src/main.jsx - 導入並包裹 ErrorBoundary
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
@@ -8,23 +6,23 @@ import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { ToastProvider } from './hooks/useToast';
-// 導入新的 ErrorBoundary
-import ErrorBoundary from './components/ErrorBoundary.jsx'; 
+// 導入錯誤邊界組件
+import ErrorBoundary from './components/ErrorBoundary.jsx'; 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // 在開發模式下，React.StrictMode 有助於捕獲潛在問題，但不建議在生產環境中移除
-  <React.StrictMode> 
-    <ErrorBoundary> {/* 將整個應用程式包裹在錯誤邊界內 */}
-        <BrowserRouter>
-            <AuthProvider>
-                <ThemeProvider>
-                    <ToastProvider>
-                        <App />
-                    </ToastProvider>
-                </ThemeProvider>
-            </AuthProvider>
-        </BrowserRouter>
-    </ErrorBoundary>
-  </React.StrictMode>,
+  // 在開發模式下，React.StrictMode 有助於捕獲潛在問題
+  <React.StrictMode> 
+    <ErrorBoundary> {/* 將整個應用程式包裹在錯誤邊界內 */}
+        <BrowserRouter>
+            <AuthProvider>
+                <ThemeProvider>
+                    <ToastProvider>
+                        <App />
+                    </ToastProvider>
+                </ThemeProvider>
+            </AuthProvider>
+        </BrowserRouter>
+    </ErrorBoundary>
+  </React.StrictMode>,
 );
