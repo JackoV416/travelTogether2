a64ee0630d2f0f250c8d47731ac16d5e16a29c40
JackoV416 <jamiekwok416@gmail.com>
2025-11-25 15:21:20 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
-import { ThemeProvider } from './contexts/ThemeContext';
-import { ToastProvider } from './hooks/useToast';
-// 導入錯誤邊界組件
-import ErrorBoundary from './components/ErrorBoundary.jsx'; 
-
+import App from './App.jsx'; // 確保這裡導入您的 App 元件
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // 在開發模式下，React.StrictMode 有助於捕獲潛在問題
-  <React.StrictMode> 
-    <ErrorBoundary> {/* 將整個應用程式包裹在錯誤邊界內 */}
-        <BrowserRouter>
-            <AuthProvider>
-                <ThemeProvider>
-                    <ToastProvider>
-                        <App />
-                    </ToastProvider>
-                </ThemeProvider>
-            </AuthProvider>
-        </BrowserRouter>
-    </ErrorBoundary>
-  </React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
