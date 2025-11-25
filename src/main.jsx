0ef5885af80941d3a6453abc20d93a850fe2b2ce
JackoV416 <jamiekwok416@gmail.com>
2025-11-25 17:01:45 +0800

    Delte all

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx'; // 確保這裡導入您的 App 元件
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
