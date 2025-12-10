cdd34e1c57bc8e2721f8ce9eff0b1c98e256a77c
JackoV416 <jamiekwok416@gmail.com>
2025-12-10 13:50:07 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 // ⚠️ 重要：請去 Firebase Console 複製你的設定並替換下面這一塊
 const firebaseConfig = {
-  apiKey: "AIzaSyBGlEoflf06E_lBi7FHnU1k2xNRN3_QBes",
-  authDomain: "travel-together2-byjamie.firebaseapp.com",
-  projectId: "travel-together2-byjamie",
-  storageBucket: "travel-together2-byjamie.firebasestorage.app",
-  messagingSenderId: "270079374388",
-  appId: "1:270079374388:web:93aeeb483945dbab7ac661",
-  measurementId: "G-WB5T9XJ42E"
+  apiKey: "REACT_APP_FIREBASE_API_KEY",
+  authDomain: "REACT_APP_FIREBASE_AUTH_DOMAIN",
+  projectId: "REACT_APP_FIREBASE_PROJECT_ID",
+  storageBucket: "REACT_APP_FIREBASE_STORAGE_BUCKET",
+  messagingSenderId: "REACT_APP_FIREBASE_MESSAGING_SENDER_ID",
+  appId: "REACT_APP_FIREBASE_APP_ID",
+  measurementId: "REACT_APP_FIREBASE_MEASUREMENT_ID"
 };
 
 const app = initializeApp(firebaseConfig);
